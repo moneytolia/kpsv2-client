6034c550f224414200e84b5808fa4b951bb0639b
Ali Eren Öztürk <oztalieren@gmail.com>
2024-06-05 17:06:52 +0300

    refactor: update pre-commit config to use ruff & format project

---
+++ kpsv2_client/config.py
@@ -0,0 +1,5 @@
+settings = {
+    "bilesik_kutuk_sorgula": "http://kps.nvi.gov.tr/2024/04/01/BilesikKutukSorgulaKimlikNoServis/Sorgula",
+    "kps_url": "https://kpsv2test.nvi.gov.tr/Services/RoutingService.svc",
+    "sts_url": "https://kimlikdogrulama.nvi.gov.tr/Services/Issuer.svc/IWSTrust13",
+}
